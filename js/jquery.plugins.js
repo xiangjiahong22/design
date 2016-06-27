c796af74d200ac54f6a5a13d36e8d861abd20f4e
向家红 <541353071@qq.com>
2016-06-27 18:05:23 +0800

    *

---
+++ js/jquery.plugins.js
@@ -0,0 +1,38 @@
+//common plugins
+(function($){
+	$().ready(function(){
+		$.fn.extend({
+			//toggle hide
+			//## .toggleCon ##
+			//## .toggle - .hide ##
+
+			//##<div class="designCenter toggleCon">
+			//##	<div class="toggle"></div>
+			//##	<div class="toggle hide"></div>
+			//##	<div class="toggle hide"></div>
+			//##</div>
+			toggleHide:function(options){
+				var defaults = {
+	                hash: false
+	            };
+	            var options = $.extend(defaults, options);
+
+				var toggleWrap=$('.toggleCon');
+				$('a',this).on('click',function(){
+
+					var $index=$(this).index();
+					$(this).addClass('active').siblings().removeClass('active');
+
+					$('.toggle',toggleWrap).addClass('hide');
+					$('.toggle',toggleWrap).eq($index).removeClass('hide');
+
+					if(options.hash){
+						window.location.hash='#'+($index+1);
+					}
+					
+				})
+			}
+
+		});
+	})
+})(jQuery);
